09cbff410eae6dbbdd89216a92b15a9835341435
bitjungle <devel@bitjungle.com>
2020-04-16 11:58:51 +0200

    dok + n -> x

--- Undervisningsopplegg/Nullpunkt_halveringsmetoden/Nullpunkt_halveringsmetoden.py
+++ Undervisningsopplegg/Nullpunkt_halveringsmetoden/Nullpunkt_halveringsmetoden.py
@@ -1,5 +1,11 @@
+''' Dette er et forslag til løsning på punkt 6 i dette undervisningsopplegget:
+https://github.com/fagstoff/Skolekoden/blob/master/Undervisningsopplegg/Nullpunkt_halveringsmetoden/Nullpunkt_halveringsmetoden.md
+Copyright (C) 2020 BITJUNGLE Rune Mathisen
+Koden er lisensiert under en GPLv3-lisens 
+Se http://www.gnu.org/licenses/gpl-3.0.html 
+'''
 def f(x):
-    '''Funksjonen som er gitt i oppgaven.'''
+    '''Funksjonen som er gitt i oppgava.'''
     return (1/4)*x**3 - (3/4)*x**2 - x + 2
 
 a = -1 # startpunkt på venstre side av nullpunktet
@@ -7,12 +13,12 @@ b = 3  # startpunkt på høyre side av nullpunktet
 TOL = 1E-9 # toleranse: hvor nærme svaret skal vi komme før vi er fornøyde?
 
 while abs(a - b) > TOL: # så lenge avstanden mellom a og b stor
-    n = (a + b)/2       # velg n midt mellom a og b
-    print('Nå er x=', n)
-    if f(a)*f(n) > 0:   # hvis produktet er større enn null
-        a = n             # sett a til n-verdien
+    x = (a + b)/2       # velg x midt mellom a og b
+    print('Nå er x=', x)
+    if f(a)*f(x) > 0:   # hvis produktet er større enn null
+        a = x             # sett a til n-verdien
     else:               # ellers
-        b = n             # sett b til n-verdien
+        b = x             # sett b til n-verdien
                         # gjenta 
 
-print("Løsningen er x=", n)
+print("Løsningen er x=", x)
