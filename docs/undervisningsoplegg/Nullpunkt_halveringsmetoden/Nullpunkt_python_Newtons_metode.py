d1956a6948e6a1e549af9132c1d66827033dac8d
fuzzbin <fuzzbin@gmail.com>
2021-08-18 10:22:50 +0200

    Ny mappe med undervisningsopplegg

---
+++ docs/undervisningsoplegg/Nullpunkt_halveringsmetoden/Nullpunkt_python_Newtons_metode.py
@@ -0,0 +1,28 @@
+''' Dette er et forslag til alternativ løsning på punkt 6 i dette undervisningsopplegget:
+https://github.com/fagstoff/Skolekoden/blob/master/Undervisningsopplegg/Nullpunkt_halveringsmetoden/Nullpunkt_halveringsmetoden.md
+Copyright (C) 2020 BITJUNGLE Rune Mathisen og FUZZBIN Tom Jarle Christiansen
+Koden er lisensiert under en GPLv3-lisens 
+Se http://www.gnu.org/licenses/gpl-3.0.html 
+'''
+
+# Definerer funksjonen fra oppgaven
+def f(x):
+    return (1/4)*x**3 - (3/4)*x**2 - x + 2
+
+# Den deriverte av funksjonen
+def df(x):
+    return (3/4)*x**2 - (3/2)*x - 1
+
+# Siste ledd i Newtons metode
+def h(x):
+    return f(x) / df(x)
+
+x_0 = -3 # Startpunkt
+n = 5 # Antall iterasjoner
+
+x_liste = [x_0] # Liste der beregningene lagres
+
+# Løkke som kjører n iterasjoner av Newtons metode
+for i in range(n):
+    x_liste.append(x_liste[-1] - h(x_liste[-1])) # Newtons metode
+    print("Startverdi: {} - Iterasjon: {} - Beregnet verdi: {}".format(x_0, i, x_liste[-1]))
---
+++ docs/undervisningsoplegg/Tallfoelger/README.md
@@ -0,0 +1,15 @@
+# Introduksjon til tall og tallfølger med Python
+## Et undervisningsopplegg for matematikk 1T
+
+* Ingen forkunnskaper i Python er nødvendig
+* Tidsbruk er 1-2 timer
+* Anbefalt programvare er https://codewith.mu/
+eller https://trinket.io/python
+
+Vis bare ett steg til elevene av gangen, slik det er angitt i 
+[koden](intro-tall-foelger.py), og gjennomfør gode diskusjoner i hvert enkelt steg. Bruk 
+nødvendig tid til refleksjon.
+
+Det er viktig at matematikken har hovedfokus, og at Python bare er et
+verktøy i dette undervisningsopplegget. Ikke gi mer programmerings- 
+undervisning enn absolutt nødvendig for å jobbe med oppgavene. 
